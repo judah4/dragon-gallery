b80055805f999b0c0ffa3c58f88762d3a3c342c5
Judah Perez <judah@cookiedragon.games>
2018-09-25 15:20:36 -0700

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Dragons
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+docker build -t judah4/dragon-gallery .
+```
+
+### Run production
+```
+docker run -it -p 8080:8080 --rm --name dragon-gallery judah4/dragon-gallery
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <DragonGallery />
+  </div>
+</template>
+
+<script>
+import DragonGallery from './components/DragonGallery.vue'
+
+export default {
+  name: 'app',
+  components: {
+      DragonGallery
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
