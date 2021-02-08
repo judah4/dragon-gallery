de8a2d8299904d3e1b39bf1e08cc8f94ac185189
Judah Perez <judah@cookiedragon.games>
2021-02-08 08:37:23 -0800

    tail description

--- src/js/dog.js
+++ src/js/dog.js
@@ -2,7 +2,7 @@
 export default {
     Name: "Untitled Dog",
     Route: "/dog",
-    Description: "The goodest girl! Decently height, slightly on the chubbier side. Large chest, digitigrade feet, Somewhat fluffy, long snout, labrador mutt.",
+    Description: "The goodest girl! Decently height, slightly on the chubbier side. Large chest, digitigrade feet, Somewhat fluffy, long snout, labrador mutt. She has a long fluffy tail. Color Markings TBD.",
     Attributes: [
         { id: "Gender", value: "Female" },
         { id: "Age", value: "26" },
