30e9f7de3c9280a669bc4d186a5a6489eb8b6e8a
Judah Perez <judah@cookiedragon.games>
2022-12-17 14:06:58 -0800

    initial otter ideas

---
+++ src/js/otter.js
@@ -0,0 +1,26 @@
+
+export default {
+    Name: "Untitled Otter",
+    //name ideas
+    /*
+    Sally
+    */
+    Route: "/otter",
+    Description: "smol otter. Flat chest, very sleek, very athletic. In College studying... Something.",
+    Attributes: [
+        { id: "Gender", value: "Female" },
+        { id: "Age", value: "22" },
+        { id: "Height", value: "3ft 8in" },
+    ],
+    Types: ["reference"],
+    Draft: true,
+    Images: [
+        /*{
+            title: 'Ref',
+            href: 'images/commission___baby_dragon_by_raizy-d6xqz3x.png',
+            description: 'Character referece',
+            type: "reference",
+        },*/
+
+    ],
+};
